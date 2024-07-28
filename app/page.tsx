2732f3872ddca7009d545fa90014bc6156a8d123
Daniel Yang <danielwzyang@gmail.com>
2024-07-28 19:03:59 -0400

    removed accidental y from navbar

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
                 <h1 className="text-4xl w-fit mb-2 font-extrabold ">shelfmates</h1>
                 <div className="flex items-center">
                     <Link href="/" className="text-l w-fit mx-6 font-medium">browse</Link>
-                    <Link href="/" className="text-l w-fit mx-6 font-medium">favorites</Link>Y
+                    <Link href="/" className="text-l w-fit mx-6 font-medium">favorites</Link>
                     <Link href="/" className="text-l w-fit mx-6 font-medium">about</Link>
                     <Link href="/" className="text-l w-fit mx-6 font-medium">contact</Link>
                 </div>
