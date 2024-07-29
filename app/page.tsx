ff9c76840660225997d63125d813a1a996e81220
Daniel Yang <danielwzyang@gmail.com>
2024-07-29 00:24:49 -0400

    finished navbar and setup pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,9 @@
-import Image from "next/image";
-import Link from "next/link";
+import Navbar from "@/components/navbar";
 
 export default function Home() {
     return (
         <div className="min-h-screen">
-            <div className="p-5 flex flex-col items-center border border-[#e0e0e0]">
-                <h1 className="text-4xl w-fit mb-2 font-extrabold ">shelfmates</h1>
-                <div className="flex items-center">
-                    <Link href="/" className="text-l w-fit mx-6 font-medium">browse</Link>
-                    <Link href="/" className="text-l w-fit mx-6 font-medium">favorites</Link>
-                    <Link href="/" className="text-l w-fit mx-6 font-medium">about</Link>
-                    <Link href="/" className="text-l w-fit mx-6 font-medium">contact</Link>
-                </div>
-            </div>
+           <Navbar />
         </div>
     );
 }
---
+++ components/navbar.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link"
+
+export default function Navbar(props: any) {
+    return (
+        <div className="p-5 flex flex-col items-center border border-[#e0e0e0]">
+            <Link href="/" className="text-4xl w-fit mb-2 font-extrabold">shelfmates</Link>
+            <div className="flex items-center ">
+                <Link href="/shop" className={"w-fit mx-6 font-medium hover:text-[#56525c] hover:rotate-[5deg]" + (props.page == "shop" ? " underline" : "")}>shop</Link>
+                <Link href="/favorited" className={"w-fit mx-6 font-medium hover:text-[#56525c] hover:rotate-[-5deg]" + (props.page == "favorited" ? " underline" : "")}>favorited</Link>
+                <Link href="/about" className={"w-fit mx-6 font-medium hover:text-[#56525c] hover:rotate-[5deg]" + (props.page == "about" ? " underline" : "")}>about</Link>
+                <Link href="/contact" className={"w-fit mx-6 font-medium hover:text-[#56525c] hover:rotate-[-5deg]" + (props.page == "contact" ? " underline" : "")}>contact</Link>
+            </div>
+        </div>
+    )
+}
